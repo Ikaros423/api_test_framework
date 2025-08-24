ea32272836800c34a61392348280ab8f94f590f9
zch <15690413+Ikaros423@user.noreply.gitee.com>
2025-08-24 16:29:53 +0800

    修改名称

--- test_cases/test_user_login.py
+++ test_cases/test_user_login.py
@@ -1,31 +1,31 @@
-from ..apis.user_api import LoginAPI
+from ..apis.user_api import UserAPI
 import pytest
 
-def test_login_success(login_api):
+def test_login_success(user_api):
     accounts = '123456'
     pwd = '10446949'
     type = 'username'
-    response = login_api.login(accounts, pwd, type)
+    response = user_api.login(accounts, pwd, type)
     assert response.status_code == 200
     response_data = response.json()
     assert response_data['msg'] == '登录成功'
     assert response_data['code'] == 0
 
-def test_login_error_password(login_api):
+def test_login_error_password(user_api):
     accounts = '123456'
     pwd = '1044694'
     type = 'username'
-    response = login_api.login(accounts, pwd, type)
+    response = user_api.login(accounts, pwd, type)
     assert response.status_code == 200
     response_data = response.json()
     assert response_data['msg'] == '密码错误'
     assert response_data['code'] == -4
 
-def test_login_error_account(login_api):
+def test_login_error_account(user_api):
     accounts = '12345'
     pwd = '1044694'
     type = 'username'
-    response = login_api.login(accounts, pwd, type)
+    response = user_api.login(accounts, pwd, type)
     assert response.status_code == 200
     response_data = response.json()
     assert response_data['msg'] == '帐号不存在'
