8fac84ad0acba737479d3005989272c823ed4c0e
张晨晖 <zch13773107361@163.com>
2025-08-25 16:18:11 +0800

    修改路径拼接方式

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,10 @@
 import yaml
+import os
 from ..common.base_path import BASE_PATH
 
 class Settings():
     def __init__(self):
-        config_path = BASE_PATH + r'\config\config.yaml'
+        config_path = os.path.join(BASE_PATH ,'config', 'config.yaml')
         with open(config_path, "r", encoding="utf-8") as f:
             self.config = yaml.safe_load(f)
             
